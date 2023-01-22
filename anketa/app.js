0e04a0d966699e310f59c8be98db2dcafcc53e98
catitgirl <natagan888@gmail.com>
2023-01-22 21:47:23 +0400

    add calculator

--- anketa/app.js
+++ anketa/app.js
@@ -10,24 +10,22 @@ class Cat {
 }
 const prototype = new Cat(bride, petName, gender, catAge, about);
 
+document.querySelector('select').addEventListener('change', function () {
 
-//add.addEventListener('click', () => {
-    document.querySelector('select').addEventListener('change', function () {
+    prototype.bride = (this).value;
 
-        prototype.bride = (this).value;
-        
-    });
+});
 add.addEventListener('click', () => {
     const catName = document.getElementById('petName').value;
-    const radiobutton=document.querySelectorAll('input[name="gender"]');
+    const radiobutton = document.querySelectorAll('input[name="gender"]');
     const catYars = document.getElementById('catAge').value;
     const catAbout = document.getElementById('about').value;
     prototype.petName = catName;
     prototype.catAge = catYars;
     prototype.about = catAbout;
-    for (let gen1 of gender){
+    for (let gen1 of gender) {
         if (gen1.checked) {
-            prototype.gender =gen1.value;
+            prototype.gender = gen1.value;
         }
     }
 
